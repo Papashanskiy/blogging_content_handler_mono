3f82b1ae0c52c015cde3c46924e3195b917299c8
papashanskiy <apashanskiy@gmail.com>
2024-01-30 00:23:01 +0600

    Init commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <ImageUploader />
+  </div>
+</template>
+
+<script>
+import ImageUploader from './components/ImageUploader.vue';
+
+export default {
+  components: {
+    ImageUploader,
+  },
+};
+</script>
+
+<style>
+/* Ваши стили здесь */
+</style>
